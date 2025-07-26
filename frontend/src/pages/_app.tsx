6d8db9ed3333f8cc7dc7bebacdbdb9a93a5c35f3
Edoardo Kadiysky <edoardok@gmail.com>
2025-07-26 19:06:57 +0300

    fix: Use minimal index page and app without providers
    - Remove all context providers temporarily
    - Use simple static HTML page
    - This isolates the issue to see if contexts are causing the problem

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,26 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import type { AppProps } from 'next/app';
-import { LanguageProvider } from '../contexts/LanguageContext';
-import { AuthProvider } from '../contexts/AuthContext';
-import { ThemeProvider } from '../contexts/ThemeContext';
-import { ExtensionHandler } from '../utils/extensionHandler';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    // Initialize extension error handler
-    if (typeof window !== 'undefined') {
-      ExtensionHandler.getInstance();
-    }
-  }, []);
-
-  return (
-    <ThemeProvider>
-      <LanguageProvider>
-        <AuthProvider>
-          <Component {...pageProps} />
-        </AuthProvider>
-      </LanguageProvider>
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 }
